d897137d39aa802e68d371e92975f5c8f357077a
Amandeep Singhe <pks95990@gmail.com>
2024-07-22 16:06:59 +0530

    lec03

---
+++ lec_03_loops,patterns/03_conditionalAndLoops.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n;
+    // cin>>n;
+
+    // cout<<"value of n is :"<<n<<endl;
+
+    //  if a is postive
+    /*
+    int a ;
+    cin>>a;
+     if(a>0)
+    {
+        cout<<"A is postive"
+        <<endl;
+    }
+    else{
+        cout<<"A is negtive"
+        <<endl;
+    }
+    */
+/*
+    int a, b;
+    cin >> a >> b; // doest read space tab or enter button fuction z
+    cout << "Value of a and b is "
+         << a << " " << b << endl;
+*/
+
+int a,b;
+a=cin.get();
+
+cout <<"vlue of a is 
++"
+}
