8853e59337e7329e3b8d1187a0cb54fd0aef27fd
Amandeep Singhe <pks95990@gmail.com>
2024-07-31 23:17:34 +0530

    rightshif adn left shift

--- lec_04_operators/BitwiseOperators.cpp
+++ lec_04_operators/BitwiseOperators.cpp
@@ -12,5 +12,9 @@ cout<<"a|b ="<< ( a|b)<<endl;
 cout<<"~a ="<< ( ~a)<<endl;
 cout<<"a^b ="<< ( a^b)<<endl;
 
+cout<<(17>>1)<<endl;
+cout<<(17>>2)<<endl;
+cout<<(19<<1)<<endl;
+cout<<(21<<1)<<endl;
 
 }
