08588292408bd65e215fa81b363d696973794fb0
Amandeep Singhe <pks95990@gmail.com>
2024-08-12 19:43:26 +0530

    array start

---
+++ lec_09_arrays/01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+// declare
+int number[15];
+
+//accesing an array
+
+cout<<"value at 15 index "<<number[14]<<endl;
+
+cout<<endl<<"everything is fine"<<endl;
+return 0;
+
+}
