d304899ea42bae64ecc0345b1d72aedc67d9c279
Amandeep Singhe <pks95990@gmail.com>
2024-08-12 21:09:42 +0530

    types of arrays

---
+++ lec_09_arrays/03_TypesOfArarays.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+//types of arrays 
+
+   char ch[5]={'a','b','c','d','e'};
+
+   cout<<ch[3]<<endl;
+
+   double firstDouble[5];
+   float firstFloat[6];
+   bool firstBool[9];
+
+
+
+
+
+
+
+   
+   cout<<endl<<"code by Amandeep Singh "<<endl<<endl; 
+
+}
