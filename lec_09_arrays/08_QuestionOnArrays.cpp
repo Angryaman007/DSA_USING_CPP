f30eafb47b93c01e63aa8d3617042c2685f9d6d4
Amandeep Singhe <pks95990@gmail.com>
2024-08-15 21:25:41 +0530

    questions

---
+++ lec_09_arrays/08_QuestionOnArrays.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+//swap an two elemnt inside and array 
+void printArray(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i] <<" ";
+    }
+    cout<<endl;
+}
+
+void swapAlternate(int arr[], int size)
+{
+    for(int i=0; i<size; i=i+2)
+    {
+
+        if(i+1<size)
+        {
+            swap(arr[i], arr[i+1]);
+        }
+    }
+}
+
+int main()
+{
+    int even[8]={5,2,9,4,7,6,1,0};
+    int odd[5]={11,33,9,76,43};
+
+    swapAlternate(even, 8);
+
+    printArray(even ,8);
+
+swapAlternate(odd,5);
+printArray(odd,5);
+
+    return 0;
+}
+
+//output will be 
+// 2 5 4 9 6 7 0 1 
+// 33 11 76 9 43
