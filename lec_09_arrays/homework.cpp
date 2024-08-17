bbbbcb97157a7f01243e2a58eaa2f4a8b1b98fff
Amandeep Singhe <pks95990@gmail.com>
2024-08-17 17:48:08 +0530

    homework 2.0

--- lec_09_arrays/homework.cpp
+++ lec_09_arrays/homework.cpp
@@ -38,6 +38,19 @@ Consider ARR = [1, 2, 3, 4, 4], the duplicate integer value present in the array
 Note :
 A duplicate number is always present in the given array.
 
+
+//Problem statement
+
+
+You are given an array/list ARR consisting of N integers. Your task is to find all the distinct triplets present in the array which adds up to a given number K.
+
+An array is said to have a triplet {ARR[i], ARR[j], ARR[k]} with sum = 'K' if there exists three indices i, j and k such that i!=j, j!=k and i!=j and ARR[i] + ARR[j] + ARR[k] = 'K'.
+
+Note:
+1. You can return the list of values in any order. For example, if a valid triplet is {1, 2, -3}, then {2, -3, 1}, {-3, 2, 1} etc is also valid triplet. Also, the ordering of different triplets can be random i.e if there are more than one valid triplets, you can return them in any order.
+2. The elements in the array need not be distinct.
+3. If no such triplet is present in the array, then return an empty list, and the output printed for such a test case will be "-1".
+
 */
 
 //leatcode promblem no 1207
