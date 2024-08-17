ebc2b09b078bfa14fdb26824164c55098e677de6
Amandeep Singhe <pks95990@gmail.com>
2024-08-17 18:07:50 +0530

    finish lect 10

--- lec_09_arrays/09_sort01.cpp
+++ lec_09_arrays/09_sort01.cpp
@@ -5,49 +5,47 @@ using namespace std;
 void printArray(int arr[], int n){
     for(int i=0; i<n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout<<arr[i] <<" ";
 
     }
-    cout<<endl;
+    cout << endl;
 }
 
 void sortOne(int arr[], int n){
     int left=0 ,right=n-1;
 
-int step=0;
-
-
 while (left<right)
 {
-    cout<<"step"<<step<<endl;
-    printArray(arr,n);
-    cout<<endl;
-    while (arr[left]==0)
+
+    while (arr[left]==0 && left<right)
     {
         left++;
     }
-    while (arr[right]==1)
+    while (arr[right]==1 && left<right)
     {
         right--;
     }
     //agar yaha pouch gye ho ,iska matlab 
     //arr[i]==1; anf arr[j]==o'
+if(left<right){
+
 
     swap(arr[left], arr[right]);
 
     left++;
     right--;
-    
+}
+
 }
 
 }
 
 int main()
 {
-    int arr[8]={1,1,0,0,0,1,0};
+    int arr[8]={1,1,0,0,0,0,1,0};
 
-    sortOne(arr,0);
-    printArray(arr,0);
+    sortOne(arr,8);
+    printArray(arr,8);
 
     
     return 0;
