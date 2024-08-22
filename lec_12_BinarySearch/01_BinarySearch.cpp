58c5125623220fa37eafe446320ef3178cef7096
Amandeep Singhe <pks95990@gmail.com>
2024-08-23 01:12:34 +0530

    .

---
+++ lec_12_BinarySearch/01_BinarySearch.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        // go to right wala part
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+    cout << "element not found" << endl;
+}
+
+int main()
+{
+
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 13, 17};
+    
+    int evenindex=binarySearch(even, 6, 20);
+    cout<<"index of 12 is "<<evenindex<<endl;
+    int oddindex=binarySearch(odd, 5, 13);
+    cout<<"index of 13 is "<<oddindex<<endl;
+
+
+    return 0;
+
+}
