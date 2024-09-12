fec0820d6d273a1f708f3886046b1bbf3f4c971d
Amandeep Singhe <pks95990@gmail.com>
2024-09-12 22:49:21 +0530

    c++stl

---
+++ lec_19_C++_STL/vectorIin_c++stl.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std; 
+
+
+int main()
+{
+    std::cout<<"hellow world duniya sana cow \n";
+
+    vector<int>v;
+    cout<<"size:: "<<v.capacity()<<endl;
+
+}
+
