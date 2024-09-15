20b5bb74cbb6b927d0a0e2bd6d7bbe47e150f13f
Amandeep Singhe <pks95990@gmail.com>
2024-09-15 14:17:00 +0530

    intro to char arrays

---
+++ lec_22_All about Chr Arrays Strings/intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+char name[20];
+cout<<"Enter your name "<<endl;
+cin>>name;
+// name[2]='\0'; this is how we give null character in the index of char array .
+cout<<"Your names is ";
+cout<<name<<endl;
+//cin does not take space or tab or enter keys it wil stop excution 
+}
