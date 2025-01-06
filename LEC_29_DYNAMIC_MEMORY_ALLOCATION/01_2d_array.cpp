2f85650427e303369a463a1445115bff71b82f51
Amandeep Singhe <pks95990@gmail.com>
2025-01-06 16:13:48 +0530

    dyanmic memory in 2d

---
+++ LEC_29_DYNAMIC_MEMORY_ALLOCATION/01_2d_array.cpp
@@ -0,0 +1,44 @@
+//how to do dyanmic memory allocation in 2d array 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int n;
+cin>>n;
+int** arr=new int*[n];
+for(int i=0; i<n; i++)
+{
+    arr[i]=new int[n];
+
+}
+
+//creation done 
+
+
+
+//taking input 
+
+for(int i=0; i<n; i++)
+{
+    for(int j=0; j<n; j++)
+    {
+        cin>>arr[i][j];
+    }
+}
+
+//taking input
+cout<<endl;
+for(int i=0; i<n; i++)
+{
+    for(int j=0; j<n; j++)
+    {
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+    
+}
+    return 0;
+}
---
+++ LEC_29_DYNAMIC_MEMORY_ALLOCATION/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+
+int** arr=new int*[n];
+for(int i=0; i<n; i++)
+{
