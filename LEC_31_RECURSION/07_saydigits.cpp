c9f60447254e87b9752c4a259dbd502cbeef1848
Amandeep Singhe <pks95990@gmail.com>
2025-01-10 23:54:12 +0530

    say digit

---
+++ LEC_31_RECURSION/07_saydigits.cpp
@@ -0,0 +1,44 @@
+
+/*
+
+question statment is 
+input is : 412
+then
+output is: fout one two 
+inpute 2 is : 122
+output is : one two two 
+
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+void sayDigit(int n, string arr[]){
+    //base case
+if(n==0)
+return ;
+
+//processing 
+int digit =n%10;
+n=n/10;
+
+
+// recursive call
+
+sayDigit(n, arr);
+cout<<arr[digit]<<" ";
+}
+
+int main()
+{
+string arr[10]={" zero","one","two", "three","four","five","six","seven","eight", "nine"};
+
+    int n;
+    cin>>n;
+
+    sayDigit(n, arr);
+
+    return 0;
+}
