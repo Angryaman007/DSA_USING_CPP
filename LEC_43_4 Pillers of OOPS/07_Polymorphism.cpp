49d1d7abd8d1a216bb008ee9bda7397e3f89ce2b
Amandeep Singhe <pks95990@gmail.com>
2025-02-17 21:29:58 +0530

    add

--- LEC_43_4 Pillers of OOPS/07_Polymorphism.cpp
+++ LEC_43_4 Pillers of OOPS/07_Polymorphism.cpp
@@ -20,10 +20,32 @@ void sayHello(string name)
 }
 };
 
+class B {
+    public:
+    int a;
+    int b;
+
+    public:
+    int add()
+    {
+        return a+b;
+    }
+    void operator+ (B &obj){
+        int value1=this->a;
+        int value2=obj.a;
+        cout<<"output "<<value2-value1<<endl;
+    }
+};
+
 
 int main()
 {
     A obj;
     obj.sayHello();
+    B obj1, obj2;
+    obj1.a=4;
+    obj2.a=7;
+
+    obj1+obj2;
     return 0;
 }
