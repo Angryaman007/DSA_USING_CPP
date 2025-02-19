2620f77af1a92a74be93262de11467a4cc31ea22
Amandeep Singhe <pks95990@gmail.com>
2025-02-19 21:29:55 +0530

    3 pillerrs of oopa khatam hoge hai

--- LEC_43_4 Pillers of OOPS/07_Polymorphism.cpp
+++ LEC_43_4 Pillers of OOPS/07_Polymorphism.cpp
@@ -34,18 +34,41 @@ class B {
         int value1=this->a;
         int value2=obj.a;
         cout<<"output "<<value2-value1<<endl;
+        cout<<"hello love babbar "<<endl;
+    }
+    void operator() ()
+    {
+         cout<<"main Bracket "<<this ->a <<endl;
     }
 };
 
-
+class Animal {
+    public:
+    void speak()
+    {
+        cout<<"Speaking "<<endl;
+    }
+};
+class Dog: public Animal{
+    public:
+    void speak()
+    {
+        cout<<"BAarkig "<<endl;
+    }
+};
+ 
 int main()
 {
-    A obj;
-    obj.sayHello();
-    B obj1, obj2;
-    obj1.a=4;
-    obj2.a=7;
+    // A obj;
+    // obj.sayHello();
+    // B obj1, obj2;
+    // obj1.a=4;
+    // obj2.a=7;
 
-    obj1+obj2;
+    // obj1+obj2;
+
+Dog obj;
+obj.speak(); 
+
     return 0;
 }
