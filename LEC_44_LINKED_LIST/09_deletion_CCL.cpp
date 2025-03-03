c0b9bb8d3ffe1d80c656a981ca58129ba3d202b6
Amandeep Singhe <pks95990@gmail.com>
2025-03-03 20:28:03 +0530

    everycases are handled.

--- LEC_44_LINKED_LIST/09_deletion_CCL.cpp
+++ LEC_44_LINKED_LIST/09_deletion_CCL.cpp
@@ -58,6 +58,11 @@ void insertNode(Node *&tail, int element, int d)
 void print(Node *tail)
 {
     Node *temp = tail;
+    if(tail==NULL)
+    {
+        cout<<"List is Empty"<<endl;
+        return;
+    }
     do
     {
         cout << tail->data << " ";
@@ -66,34 +71,42 @@ void print(Node *tail)
     cout << endl;
 }
 
-//Deletion in circular list or CCL  
-void deleteNode(Node* &tail, int value )
+// Deletion in circular list or CCL
+void deleteNode(Node *&tail, int value)
 {
-//if list is EMPTY
-if(tail==NULL)
-{
-    cout<<" list is empty , please check again " <<endl;
-    return;
-}
-else{
-    //if list is NON-EMPTY
-    //assuming that "value " is present in the linked list 
-    Node* prev=tail;
-    Node* curr=prev->next;
+    // if list is EMPTY
+    if (tail == NULL)
+    {
+        cout << " list is empty , please check again " << endl;
+        return;
+    }
+    else
+    {
+        // if list is NON-EMPTY
+        // assuming that "value " is present in the linked list
+        Node *prev = tail;
+        Node *curr = prev->next;
 
-    while(curr->data != value)
-    {
-        prev=curr;
-        curr=curr->next;
+        while (curr->data != value)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = curr->next;
+        // only 1 node linkedlist then do this
+        if (curr == prev)
+        {
+            tail = NULL;
+        }
+
+        // for linked list greater or equal to 2
+        else if (tail == curr)
+        {
+            tail = prev;
+        }
+        curr->next = NULL;
+        delete curr;
     }
-    prev->next=curr->next;
-    if(tail==curr)
-    {
-        tail=prev;
-    }
-    curr->next=NULL;
-    delete curr;
-}
 }
 int main()
 {
@@ -101,25 +114,25 @@ int main()
     // empty list me insert karre hai
     insertNode(tail, 5, 3);
     print(tail);
+    /*
+        insertNode(tail, 3, 5);
+        print(tail);
 
-    insertNode(tail, 3, 5);
-    print(tail);
+        insertNode(tail, 5, 7);
+        print(tail);
 
-    insertNode(tail, 5, 7);
-    print(tail);
+        insertNode(tail, 7, 9);
+        print(tail);
 
-    insertNode(tail, 7, 9);
-    print(tail);
+        insertNode(tail, 5, 6);
+        print(tail);
 
-    insertNode(tail, 5, 6);
-    print(tail);
+        insertNode(tail, 9, 10);
+        print(tail);
 
-    insertNode(tail, 9, 10);
-    print(tail);
-
-    insertNode(tail, 3, 4);
-    print(tail);
-
+        insertNode(tail, 3, 4);
+        print(tail);
+    */
     deleteNode(tail, 3);
     print(tail);
     return 0;
