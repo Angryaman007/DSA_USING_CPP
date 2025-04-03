315d338b8b7d7b97f155bdfe5f1f280dba6ce5c8
Amandeep Singhe <pks95990@gmail.com>
2025-04-04 01:06:41 +0530

    sort stack

--- LEC_54_STACKS/07_sort_stack.cpp
+++ LEC_54_STACKS/07_sort_stack.cpp
@@ -9,7 +9,7 @@ push(S) : Adds a new element to the stack.
 pop(S) : Removes top element from the stack.
 top(S) : Returns value of the top element. Note that this function does not remove elements from the stack.
 Note :
-1) Use of any loop constructs like while, for..etc is not allowed. 
+1) Use of any loop constructs like while, for..etc is not allowed.
 2) The stack may contain duplicate integers.
 3) The stack may contain any integer i.e it may either be negative, positive or zero.
 Detailed explanation ( Input/output format, Notes, Images )
@@ -28,7 +28,7 @@ Sample Input 1:
 Sample Output 1:
 9 5 3 -2 -7
 Explanation of Sample Input 1:
-9 Is the largest element, hence it’s present at the top. Similarly 5>3, 3>-2 and -7 being the smallest element is present at the last. 
+9 Is the largest element, hence it’s present at the top. Similarly 5>3, 3>-2 and -7 being the smallest element is present at the last.
 Sample Input 2:
 1
 5
@@ -36,7 +36,7 @@ Sample Input 2:
 Sample Output 2:
 30 18 14 -3 -5
 Explanation of Sample Input 2:
-30 is the largest element, hence it’s present at the top. Similarly, 18>14, 14>-3 and -5 being the smallest element is present at the last. 
+30 is the largest element, hence it’s present at the top. Similarly, 18>14, 14>-3 and -5 being the smallest element is present at the last.
 
 */
 
@@ -44,9 +44,77 @@ Explanation of Sample Input 2:
 #include <stack>
 using namespace std;
 
+// fuction for sortedinseret
 
+void sortedInsert(stack<int> &stack, int num)
+{
+	// base case
+	if (stack.empty() || (!stack.empty() && stack.top() < num))
+	{
+		stack.push(num);
+		return;
+	}
+
+	int n = stack.top();
+	stack.pop();
+	// recusive CAll
+	sortedInsert(stack, num);
+	stack.push(n);
+}
 
 void sortStack(stack<int> &stack)
 {
 	// Write your code here
+
+	//base case 
+	if(stack.empty())
+	{
+		return;
+	}
+	int num=stack.top();
+stack.pop();
+
+//recusive Call 
+sortStack(stack);
+sortedInsert(stack,num);
+}
+
+// Function to print the stack
+void printStack(stack<int> s) {
+    stack<int> temp;
+    
+    // Reverse the stack to print in correct order
+    while (!s.empty()) {
+        temp.push(s.top());
+        s.pop();
+    }
+    
+    while (!temp.empty()) {
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+    cout << endl;
+}
+
+// Main function
+int main() {
+    stack<int> s;
+
+    // Pushing elements into the stack (unsorted)
+    s.push(4);
+    s.push(2);
+    s.push(8);
+    s.push(1);
+    s.push(3);
+
+    cout << "Original Stack: ";
+    printStack(s);
+
+    // Sort the stack
+    sortStack(s);
+
+    cout << "Sorted Stack: ";
+    printStack(s);
+
+    return 0;
 }
